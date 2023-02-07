3e8195eef38d8319794ec590b16cfe3d6421bef8
mehLabs <hugo.sud.ste@gmail.com>
2023-02-06 21:31:26 -0300

    remove boilerplate code

--- backend/src/server/app.ts
+++ backend/src/server/app.ts
@@ -5,12 +5,6 @@ const router: Router = express.Router();
 
 const app: Application = express();
 
-process.on("uncaughtException", function (exception) {
-  console.log(exception);
-});
-process.on("unhandledRejection", (reason, p) => {
-  console.log("Unhandled Rejection at: Promise ", p, " reason: ", reason);
-});
 app.use(express.json());
 app.use(cors());
 app.use("/api", routes(router));
